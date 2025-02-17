24cc1653fcbdcdee64c8aff0ebdb91be3c2672da
Никита Феклистов <nikoto07@gmail.com>
2025-02-17 21:53:45 +0300

    Initial commit

---
+++ go.mod.go
@@ -0,0 +1,16 @@
+module github.com/GolangLessons/protos
+
+go 1.21
+
+require (
+google.golang.org/grpc v1.58.0
+google.golang.org/protobuf v1.31.0
+)
+
+require (
+github.com/golang/protobuf v1.5.3
+golang.org/x/net v0.12.0
+golang.org/x/sys v0.10.0
+golang.org/x/text v0.11.0
+google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98
+)
